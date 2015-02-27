e9f90664bceb6d0748c5f15e7a7405bb64b72708
Lars Helge Overland <larshelge@gmail.com>
2015-02-27 09:36:22 +0100

    revno: 18215 message:   R 18441
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: 2.18
    timestamp: Fri 2015-02-27 09:36:22 +0100

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
@@ -308,7 +308,7 @@ public class JdbcEventAnalyticsTableManager
         String sql = 
             "select distinct(extract(year from psi.executiondate)) " +
             "from programstageinstance psi " +
-            "where psi.executiondate is not null";
+            "where psi.executiondate is not null ";
 
         if ( earliest != null )
         {
