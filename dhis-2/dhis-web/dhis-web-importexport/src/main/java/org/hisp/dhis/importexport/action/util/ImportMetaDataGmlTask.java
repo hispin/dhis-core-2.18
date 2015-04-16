24b26e65fba09afa5f3aa491971057ea0abf9681
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2015-04-16 12:58:08 +0200

    revno: 18370 message:   R 18884
    committer: Halvdan Hoem Grelland <halvdanhg@gmail.com>
    branch nick: 2.18
    timestamp: Thu 2015-04-16 12:58:08 +0200

--- dhis-2/dhis-web/dhis-web-importexport/src/main/java/org/hisp/dhis/importexport/action/util/ImportMetaDataGmlTask.java
+++ dhis-2/dhis-web/dhis-web-importexport/src/main/java/org/hisp/dhis/importexport/action/util/ImportMetaDataGmlTask.java
@@ -30,8 +30,9 @@ package org.hisp.dhis.importexport.action.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hisp.dhis.dxf2.gml.GmlImportService;
 import org.hisp.dhis.dxf2.metadata.ImportOptions;
+import org.hisp.dhis.dxf2.gml.GmlImportService;
+
 
 import org.hisp.dhis.scheduling.TaskId;
 
@@ -91,6 +92,8 @@ public class ImportMetaDataGmlTask
         catch ( IOException | TransformerException e )
         {
             log.error( "Unable to read GML data from input stream", e );
+
+            throw new RuntimeException( "Failed to parse GML input stream", e );
         }
     }
 }
