796246567cd9f6179970bf516fd15c5ce3e3b056
Lars Helge Overland <larshelge@gmail.com>
2015-04-22 20:06:43 +0200

    revno: 18394 message:   R 18988
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: 2.18
    timestamp: Wed 2015-04-22 20:06:43 +0200

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/datavalueset/DefaultDataValueSetService.java
@@ -792,7 +792,7 @@ public class DefaultDataValueSetService
 
             if ( valueValid != null )
             {
-                summary.getConflicts().add( new ImportConflict( dataValue.getValue(), i18n.getString( valueValid ) + ", must match data element type: " + dataElement ) );
+                summary.getConflicts().add( new ImportConflict( dataValue.getValue(), i18n.getString( valueValid ) + ", must match data element type: " + dataElement.getUid() ) );
                 continue;
             }
 
@@ -846,7 +846,7 @@ public class DefaultDataValueSetService
             
             if ( zeroInsignificant )
             {
-                summary.getConflicts().add( new ImportConflict( internalValue.getValue(), "Value is zero and not significant, must match data element: " + dataElement ) );
+                summary.getConflicts().add( new ImportConflict( internalValue.getValue(), "Value is zero and not significant, must match data element: " + dataElement.getUid() ) );
                 continue;
             }
 
